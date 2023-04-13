e73db71caec3f5181fda10e6a2cca5475e61fbc1
Ajay Chauhan <48796206+mr-ajaychauhan@users.noreply.github.com>
2023-04-13 15:30:22 +0000

    version 4 done node postgresql

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,9 +5,9 @@ const router = require('express').Router();
 
 router
     .get('/', controller.getAll)
-    // .get('/:id',controller.getOne)
-    // .post('/',controller.createOne)
-    // .put('/:id',controller.updateOne)
-    // .delete('/:id',controller.deleteOne)
+    .get('/:id',controller.getOne)
+    .post('/',controller.createOne)
+    .put('/:id',controller.updateOne)
+    .delete('/:id',controller.deleteOne)
 
 module.exports = router;
